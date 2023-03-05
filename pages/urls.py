cfe3995c750aae82dd3abac7bb33843eda2243c4
John Sarandi Campilla <76666778+johncampilla@users.noreply.github.com>
2023-03-05 08:37:45 +0800

    First Commit

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contacts', views.contacts, name='contacts'),
+
 ]
